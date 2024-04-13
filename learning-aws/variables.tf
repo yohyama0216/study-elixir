7ec187295e9a2f1601f35f2dbdf39ae5867ee956
yohyama <13836268+yohyama0216@users.noreply.github.com>
2024-04-13 10:54:35 +0900

    Create variables.tf

---
+++ learning-aws/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "The VPC ID where resources will be created"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The Subnet ID where instances will be created"
+  type        = string
+}
